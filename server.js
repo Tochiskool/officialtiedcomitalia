1a339477318a1f3385afbe4023e40ceeadbbfd1e
Tochiskool <mrchristian79@yahoo.com>
2021-04-12 20:39:15 +0200

    Decisions on how to structure the back end between mongodb and mysql

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
+//Dependencies
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
-const bodyParser = require("body-parser");
 const path = require("path");
+
+//Thinking of mysql backend
+const mysql = require('mysql');
+
+//Create a connection to the database
+//Mongo || mysql workbench db
+
+
+//Door to my app at 8080
 const PORT = process.env.PORT || 8080;
 
+//Creating the app server
 const app = express();
+
+//Handling Data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //HTTP Request Loger
 app.use(morgan("tiny"));
@@ -14,5 +28,5 @@ app.use(morgan("tiny"));
 if (process.env.NODE_ENV === "production") {
    app.use(express.static("client/build"));
 }
-
+//Start listening to what the app has to offer
 app.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
